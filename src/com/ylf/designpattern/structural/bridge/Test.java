deedd54b6df599ab7cef1df4475c116c4713e97a
Jerry <ylf989@163.com>
2017-09-04 23:43:40 +0800

    modify

--- src/com/ylf/designpattern/structural/bridge/Test.java
+++ src/com/ylf/designpattern/structural/bridge/Test.java
@@ -3,7 +3,7 @@ package com.ylf.designpattern.structural.bridge;
 /**
  * 
  * 桥接模式
- * 优点：通过组合的方式将类的抽象部分和实现部分分离，使类的设计遵循单一职责原则
+ * 优点：通过组合的方式将类的（一个或多个）抽象部分和（一个或多个）实现部分分离，使类的设计遵循单一职责原则
  * 使用场景：当类有多个维度会引起类的变化时，若采用继承的方式实现，会生成多个维度各自实现数相乘数量的子类，且每个子类职责不单一
  * 
  * @author Jerry
@@ -12,11 +12,15 @@ package com.ylf.designpattern.structural.bridge;
 public class Test {
 
 	public static void main(String[] args) {
-		Color color = new RedColor();
-		Shape shape = new CircleShape();
+		//选择抽象部分的实现为小画笔
 		Brush brush = new SmallBrush();
+		//选择实现部分的颜色维度的实现为红色
+		Color color = new RedColor();
 		brush.setColor(color);
+		//选择实现部分的形状维度的实现为圆形
+		Shape shape = new CircleShape();
 		brush.setShape(shape);
+		
 		brush.draw();
 	}
 
