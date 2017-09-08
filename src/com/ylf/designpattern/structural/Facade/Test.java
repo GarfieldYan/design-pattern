259e2d28b1f7eeb932046e347e1707b3b6d26eb9
Jerry <ylf989@163.com>
2017-09-08 20:16:55 +0800

    add

--- src/com/ylf/designpattern/structural/Facade/Test.java
+++ src/com/ylf/designpattern/structural/Facade/Test.java
@@ -1,9 +1,15 @@
 package com.ylf.designpattern.structural.Facade;
 
+/**
+ * 门面模式，为子系统中的一组复杂接口提供一个简单的访问方式
+ * @author Jerry
+ *
+ */
 public class Test {
 
 	public static void main(String[] args) {
-
+		DrawerFacade drawer = new DrawerFacade();
+		drawer.getImportantFile();
 	}
 
 }
